5ef920b2442a050da4507ff8e23d5e6e9bc08c56
ElMargaritox <alexisguzman0303@gmail.com>
2021-09-02 19:32:41 -0300

    first commit

---
+++ AnimalSpawner/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using SDG.Unturned;
+
+namespace AnimalSpawner
+{
+    public class Class1 : RocketPlugin
+    {
+
+        protected override void Load()
+        {
+            base.Load();
+            
+
+
+        }
+
+
+
+        protected override void Unload()
+        {
+            base.Unload();
+        }
+
+        
+    }
+}
---
+++ AnimalSpawner/CommandAnimal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+
+
+namespace AnimalSpawner
+{
+    class CommandAnimal : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "summon";
+
+        public string Help => "Como En El Minecraft";
+
+        public string Syntax => string.Empty;
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            
+
+
+            int cantidad = int.Parse(command[1]);
+
+
+
+            
+
+
+            
+           
+            
+          
+            ThreadPool.QueueUserWorkItem(delegate (object item)
+            {
+                UnturnedPlayer player = (UnturnedPlayer)caller;
+
+               
+
+                while (cantidad > 0)
+                {
+                    Thread.Sleep(1000);
+                    Animal animal = AnimalManager.getAnimal(ushort.Parse(command[0]));
+                    AnimalManager.spawnAnimal(animal.index, player.Player.look.aim.position, UnityEngine.Quaternion.AngleAxis(1f, player.Player.look.aim.position));
+                    cantidad--;
+                    
+                }
+            });
+            
+            
+
+            
+            
+        }
+    }
+}
